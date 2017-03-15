321a27c09fa2eaa98ee2e80dc98bec49ff43d155
vovanluan <vovanluan201294@gmail.com>
2017-03-15 16:15:47 +0700

    Add user validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,13 @@
 class User < ActiveRecord::Base
-	validates :email, presence: true, length: {minimum: 10, maximum: 30}
+	validates :username, presence: true, 
+				uniqueness: {case_sensitive: false},
+				length: {minimum: 3, maximum: 25}	
+	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i				
+	validates :email, presence: true,
+				uniqueness: {case_sensitive: false},
+				length: {minimum: 8, maximum: 30},
+				format: { with: VALID_EMAIL_REGEX }
 	validates :password, presence: true, length: {minimum: 8, maximum: 16}
+
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170315044326) do
+ActiveRecord::Schema.define(version: 20170315082209) do
 
   create_table "users", force: :cascade do |t|
     t.string "email"
     t.string "password"
+    t.string "username"
   end
 
 end
