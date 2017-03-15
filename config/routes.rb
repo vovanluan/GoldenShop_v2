2d553f9ee495edbe11fb2f17f54c1f601894316a
vovanluan <vovanluan201294@gmail.com>
2017-03-15 17:59:28 +0700

    Update sign up page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root "pages#home"
-  resources :users
+  resources :users, except: [:new]
+
+  get 'signup', to: 'users#new'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
