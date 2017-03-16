8c248198bccdace7fb9961552fb57a7532756b9c
vovanluan <vovanluan201294@gmail.com>
2017-03-16 17:40:41 +0700

    Update authentication view and add page for selling a book

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,49 @@
+class BooksController < ApplicationController
+	before_action :set_Book, only: [:edit, :update, :show, :destroy]
+	def index
+		@books = Book.all
+	end	
+	def new
+		@book = Book.new
+	end
+
+	def create
+		@book = Book.new(book_params)
+		if @book.save
+			flash[:notice] = "Book was successfully created"
+			redirect_to book_path(@book)	
+		else
+			render 'new'
+		end
+
+	end
+	def show
+	end
+
+
+	def destroy 
+		@book.destroy
+		flash[:notice] = "Book was successfully deleted"
+		redirect_to Books_path
+	end
+
+	def edit
+	end
+	def update
+		if @book.update(book_params)
+			flash[:notice] = "Book was successfully updated"
+			redirect_to Book_path(@book)
+		else
+			render 'edit'
+		end
+	end
+	private
+		def book_params
+			params.require(:book).permit(:title, :description, :price, category_ids: [])
+		end
+		def set_Book
+			@book = Book.find(params[:id])			
+		end
+
+
+end
--- app/models/book.rb
+++ app/models/book.rb
@@ -2,4 +2,7 @@ class Book < ActiveRecord::Base
   belongs_to :user
   has_many :book_categories
   has_many :categories, through: :book_categories
+	validates :title, presence: true, length: { minimum: 3, maximum: 50}
+	validates :description, presence: true, length: { minimum: 10, maximum: 120}
+	validates :price, presence: true
 end
