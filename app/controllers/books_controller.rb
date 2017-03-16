b3fb01d0adee9e223161881d7243f7fa488e01c8
vovanluan <vovanluan201294@gmail.com>
2017-03-16 18:00:45 +0700

    Update adding book page

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -9,6 +9,7 @@ class BooksController < ApplicationController
 
 	def create
 		@book = Book.new(book_params)
+		@book.user = current_user
 		if @book.save
 			flash[:notice] = "Book was successfully created"
 			redirect_to book_path(@book)	
