0496676d848361f451738229936f89c54b5c6092
vovanluan <vovanluan201294@gmail.com>
2017-03-20 22:15:28 +0700

    Use custom views and controllers in devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    :sessions => "users/sessions",
+    :passwords => "users/passwords",
+    :registrations => "users/registrations"
+  }
+
   root "pages#home"
 
   resources :books, except: [:destroy]
-
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
